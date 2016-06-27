0bab8a3e2defb66ddd8f8d410aeb10c7ff39153b
Winston Yee <winsyee@gmail.com>
2016-06-26 22:12:54 -0700

    In the middle of converting async.auto to async.waterfall

--- server.js
+++ server.js
@@ -1,66 +1,148 @@
 const request = require('request');
+const async = require('async');
 
 var url = `https://www.eventbriteapi.com/v3/events/search/?q=hackathons&location.address=sanfrancisco&location.within=50mi&sort_by=date&token=${process.env.EVENTBRITE_TOKEN}`;
 
-var requestOptions = {
-  url: url,
-  json:true
-};
+async.waterfall([
+    function(waterCb) {
+      var requestOptions = {
+        url: url,
+        json:true
+      };
 
-request(requestOptions, (err, res, body) => {
-  var events;
+    request(requestOptions, (err, res, body) => {
+      var events = [];
 
-  if (err) {
-    throw err;
-  }
+      if (err) {
+        return autoCb(err);
+      }
 
-  events = body.events.forEach((event) => {
+      events = body.events.map((event) => {
+        return {
+          id: event.id,
+          name: event.name.text,
+          url: event.url,
+          description: event.description,
+          start: event.start.local,
+          end: event.end.local,
+          currency: event.currency,
+          venue_id: event.venue_id
+        };
+      });
 
+      waterCb(null, events);
+    });
+    },
+    function(events, callback) {
+      var venue = [];
+
+      async.forEach(result.getEvents, (event, eachCb) => {
+        var requestOptions = {
+          url: `https://www.eventbriteapi.com/v3/venues/${event.venue_id}/?token=${process.env.EVENTBRITE_TOKEN}`,
+          json: true
+        };
+
+        request(requestOptions, (err, res, body) => {
+          if (err) {
+            throw err;
+          }
+
+          venue = {
+            name: body.name,
+            address: body.address
+          };
+
+          eachCb()
+        });
+          autoCb(null, venue);
+      });
+    },
+    function(arg1, callback) {
+        // arg1 now equals 'three'
+        callback(null, 'done');
+    }
+], function (err, result) {
+    // result now equals 'done'
+});
+
+async.auto({
+  getEvents: (autoCb) => {
     var requestOptions = {
-      url: `https://www.eventbriteapi.com/v3/events/${event.id}/ticket_classes/?token=${process.env.EVENTBRITE_TOKEN}`,
-      json: true
-    };
-
-    var requestOptions = {
-      url: `https://www.eventbriteapi.com/v3/venues/${event.venue_id}/?token=${process.env.EVENTBRITE_TOKEN}`,
-      json: true
+      url: url,
+      json:true
     };
 
     request(requestOptions, (err, res, body) => {
-      console.log(body);
+      var events = [];
+
+      if (err) {
+        return autoCb(err);
+      }
+
+      events = body.events.map((event) => {
+        return {
+          id: event.id,
+          name: event.name.text,
+          url: event.url,
+          description: event.description,
+          start: event.start.local,
+          end: event.end.local,
+          currency: event.currency,
+          venue_id: event.venue_id
+        };
+      });
+
+      autoCb(null, events);
     });
+  },
+  getVenues: ['getEvents', (result, autoCb) => {
+    var venue = [];
 
-    // request(requestOptions, (err, res, body) => {
-    //   var tickets = body.ticket_classes.map((ticket) => {
-    //     return {
-    //       cost: ticket.cost,
-    //       fee: ticket.fee,
-    //       tax: ticket.cost,
-    //       id: event.id,
-    //       name: event.name.text,
-    //       url: event.url,
-    //       // description: event.description,
-    //       start: event.start.local,
-    //       end: event.end.local,
-    //       currency: event.currency
-    //     };
-    //   });
+    async.forEach(result.getEvents, (event, eachCb) => {
+      var requestOptions = {
+        url: `https://www.eventbriteapi.com/v3/venues/${event.venue_id}/?token=${process.env.EVENTBRITE_TOKEN}`,
+        json: true
+      };
 
-    //   console.log(tickets);
-    // });
+      request(requestOptions, (err, res, body) => {
+        if (err) {
+          throw err;
+        }
 
-  });
+        venue = {
+          name: body.name,
+          address: body.address
+        };
 
-  // events = body.events.map((event) => {
-  //   return {
-  //     id: event.id,
-  //     name: event.name.text,
-  //     url: event.url,
-  //     // description: event.description,
-  //     start: event.start.local,
-  //     end: event.end.local,
-  //     currency: event.currency
-  //   };
-  // });
-  // console.log(events);
+        eachCb()
+      });
+        autoCb(null, venue);
+    });
+  }],
+  // getTickets: ['getEvents', 'getVenues', (result, autoCb) => {
+  //   var tickets = [];
+
+  //   result.getEvents.forEach((event) => {
+  //     var requestOptions = {
+  //       url: `https://www.eventbriteapi.com/v3/events/${event.id}/ticket_classes/?token=${process.env.EVENTBRITE_TOKEN}`,
+  //       json: true
+  //     };
+
+  //     request(requestOptions, (err, res, body) => {
+  //       if (err) {
+  //         // return autoCb(err);
+  //       }
+
+  //       tickets = body.ticket_classes.map((ticket) => {
+  //         return {
+  //           cost: ticket.cost,
+  //           fee: ticket.fee,
+  //           tax: ticket.cost,
+  //         };
+  //       });
+
+  //       // autoCb(null, tickets);
+  //     });
+  //   });
+  // }]
 });
